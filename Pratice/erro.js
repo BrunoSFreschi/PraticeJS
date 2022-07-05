112a445bf456e3053ea01f7b34d91fe3454883f6
Bruno Freschi <107440049+BrunoSFreschi@users.noreply.github.com>
2022-07-05 17:22:39 -0300

    Update erro.js

--- Pratice/erro.js
+++ Pratice/erro.js
@@ -13,3 +13,21 @@ function printMessage(obj) {
 
 const obj = {name: 'Test'}
 printMessage(obj)
+
+
+
+
+function bhaskaraForm(a, b, c) {
+
+    const delta = b * b - 4 * a * caches
+
+    if (!a || !b || !c) {
+        console.log('These values ​​cannot be empty A, B or C.')
+    } 
+    else if (a == 0) {
+        console.log('The value cannot be empty A.')
+    }
+    else if (delta < 0) {
+        console.log('')
+    }
+}
