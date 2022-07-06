dfa2b9c4ee483532ca08343d63e93497440e0bba
Bruno Freschi <bruno.freschi2@gmail.com>
2022-07-06 00:09:50 -0300

    7DaysOfCode4/7

--- 7DaysOfCode/day4.js
+++ 7DaysOfCode/day4.js
@@ -1,16 +1,21 @@
 // #7DaysOfCode - Lógica JS 4/7: Loops e randomização
 
-let teste = Math.floor(Math.random() * 11)
+let randomNumber = Math.floor(Math.random() * 11)
 
+const n = 4
 
 function discoverNumber(n) {
-
-    for (let i = 0; i <= 3; i++) {
-        if (n == teste) {
+    for (let i = 1; i <= 3; i++) {
+        if (n == randomNumber) {
+            console.log(`Nice the random number is ${randomNumber}.`)
             break
         }
-        
+        console.log(`You can try more ${ 4 - i} time`)
+    }
+    if (n != randomNumber) {
+        console.log(`Ok the random number is ${randomNumber}, try again.`)       
     }
 }
 
 
+discoverNumber(n)
