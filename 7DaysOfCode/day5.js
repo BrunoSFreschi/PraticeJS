fa21d16d95c8753a173922b36efb36d39c234b4f
Bruno Freschi <bruno.freschi2@gmail.com>
2022-07-06 23:33:51 -0300

    4/77DaysOfCode

---
+++ 7DaysOfCode/day5.js
@@ -0,0 +1,19 @@
+// #7DaysOfCode - Lógica JS 4/7: Array e coleções
+
+let listBuy = {
+    Fruit: [
+        'banana',   //1
+        'aple',     //2
+        'orange'    //3
+    ],
+    Freeze: [
+        'pizza',
+        'ice cream',
+        'lasagna',
+    ],
+}
+
+listBuy.Fruit.push('water melon')   //4
+
+console.log(listBuy.Freeze[0], listBuy.Freeze[1], listBuy.Freeze[2])
+console.log(listBuy.Fruit[0], listBuy.Fruit[1], listBuy.Fruit[2], listBuy.Fruit[3])
