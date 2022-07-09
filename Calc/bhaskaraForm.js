a65a8d5711edd05aba2f0c6d3ef72322b8409151
Bruno Freschi <bruno.freschi2@gmail.com>
2022-07-08 21:54:42 -0300

    RepoUpdate

--- Calc/bhaskaraForm.js
+++ Calc/bhaskaraForm.js
@@ -1,8 +1,5 @@
-
-
 function bhaskaraForm(a, b, c) {
-
-    const delta = b * b - 4 * a * caches
+    const delta = b * b - 4 * a * c
 
     if (!a || !b || !c) {
         console.log('These values ​​cannot be empty A, B or C.')
@@ -13,4 +10,13 @@ function bhaskaraForm(a, b, c) {
     else if (delta < 0) {
         console.log('')
     }
+    else{
+        const x1 = (-b + Math.sqrt(delta)) / (2 * a)
+        const x2 = (-b - Math.sqrt(delta)) / (2 * a)
+
+        console.log(x1)
+        console.log(x2)
+    }
 }
+
+bhaskaraForm(5, 50, 5)
