96b085a9d847791187529f020c616fd59a728a14
Bruno Freschi <bruno.freschi2@gmail.com>
2022-07-09 00:51:19 -0300

    Trigonometry

--- Calc/TrigonometrcRatios.js
+++ Calc/TrigonometrcRatios.js
@@ -1,2 +1,23 @@
+//Incomplete
 
-    
+Trigonometry = {
+    Sine(opposite, hypotenuse) {
+        const sine = Math.sin(opposite / hypotenuse)
+
+        console.log(`Sine value: ${sine}`)
+    },
+
+    Cosine(adjacent, hypotenuse) {
+        const cosine = Math.cos(adjacent / hypotenuse)
+
+        console.log(`Cosine value: ${cosine}`)
+    },
+
+    Tanget(opposite, adjacent) {
+        const tanget = Math.tan(opposite / adjacent)
+
+        console.log(`Tanget value: ${tanget}`)
+    }
+}
+
+Trigonometry.Cosine(10, 5)
