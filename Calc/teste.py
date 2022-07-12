6bccfae12046f0cf2d341fd373a351d7af6b530c
Bruno Freschi <bruno.freschi2@gmail.com>
2022-07-12 00:51:42 -0300

    PythonTeste

---
+++ Calc/teste.py
@@ -0,0 +1,15 @@
+
+
+from ast import While
+from shutil import which
+
+
+a = input('')
+b = input('')
+
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+elif a > b:
+    print('a is greater than b')
