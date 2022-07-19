0fcb8bf1dfc26d9200ed23fb6381a9648ac1c71e
Bruno Freschi <bruno.freschi2@gmail.com>
2022-07-18 22:45:39 -0300

    Updadte Sphere

---
+++ Calc/VolumeOfSphere.js
@@ -0,0 +1,8 @@
+
+
+/*
+Given diameter: r = d / 2r=d/2,
+Given area: r = \sqrt{A / (4 \times π)}r= A/(4×π)
+Given volume: r = \sqrt[3]{3 \times V / (4 \times π)}r= 3 3×V/(4×π)
+Given surface to volume ratio: r = 3 / (A/V)r=3/(A/V).
+ */
